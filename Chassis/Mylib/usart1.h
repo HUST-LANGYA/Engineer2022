01e5acf4f98e5411ab0bd3ec785db89d331ff76c
zkwhust <2428419501@qq.com>
2022-05-19 23:48:51 +0800

    增加了USART1裁判系统，但是还没上车调试，不能用

--- Chassis/Mylib/usart1.h
+++ Chassis/Mylib/usart1.h
@@ -2,10 +2,10 @@
 #define _USART1_H
 #include "main.h"
 
-//#define JudgeBufBiggestSize 45	//接收数据最大长度
-//#define SEND_MAX_SIZE    128    //上传数据最大的长度
+#define JudgeBufBiggestSize 45	//接收数据最大长度
+#define SEND_MAX_SIZE    128    //上传数据最大的长度
 
-//void UART4_Configuration(void);
-//void JudgeBuffReceive(unsigned char ReceiveBuffer[],uint16_t DataLen);
+void UART4_Configuration(void);
+void JudgeBuffReceive(unsigned char ReceiveBuffer[],uint16_t DataLen);
 
 #endif
